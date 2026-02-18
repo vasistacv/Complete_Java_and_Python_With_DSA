06af3859489d398f11d92d04db8a7ee34fa0b19a
vasistacv <vasisthamanju796@gmail.com>
2026-02-18 10:00:00 +0530

    Day10 Hollow Diamond Pattern

---
+++ Java/Day10_HollowDiamond.java
@@ -0,0 +1,40 @@
+
+/**
+ * Day 10: Hollow Diamond Pattern
+ * Printing a diamond shape but only the borders.
+ */
+import java.util.*;
+
+public class Day10_HollowDiamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
