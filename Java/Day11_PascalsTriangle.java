1f810519528c6c7bfd95b3db5bce83f5f1b6926c
vasistacv <vasisthamanju796@gmail.com>
2026-02-21 17:15:00 +0530

    Day11 Pascals Triangle

---
+++ Java/Day11_PascalsTriangle.java
@@ -0,0 +1,28 @@
+
+/**
+ * Day 11: Pascal's Triangle
+ * Printing Pascal's Triangle using binomial coefficients.
+ */
+import java.util.*;
+
+public class Day11_PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            // Print spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
